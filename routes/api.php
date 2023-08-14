fe3ea74f8c8903ea9638466b0aa87243ac3c946f
lekhuong1906 <lekhuong190602@gmail.com>
2023-08-14 09:20:41 +0700

    14/08

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AddressController;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\CartController;
 use App\Http\Controllers\Api\SliderController;
@@ -42,7 +43,7 @@ Route::middleware(['auth:sanctum','customer'])->group(function () {
     Route::get('cart',[CartController::class,'showCart']);
     Route::post('add-to-cart',[CartController::class,'addToCart']);
     Route::post('update-cart',[CartController::class,'updateCart']);
-
+    Route::resource('address',AddressController::class)->except('create','edit');
 });
 
 
@@ -51,9 +52,25 @@ Route::resource('products', ProductController::class)->only('index', 'store', 's
 Route::resource('types', TypeController::class)->except('create', 'edit');
 Route::resource('sliders', SliderController::class)->only('index', 'store');
 
+use App\Models\ImageProduct;
+Route::get('test',function (){
 
+   $images = ImageProduct::get();
+    foreach ($images as $image){
+        $image_links = explode(',',$image->image_link);
+        $a = [];
+        foreach ($image_links as $image_link){
+            $head = explode('blog.test/',$image_link);
+            $a[] = $head[0].'blog.test:8080/'.$head[1];
 
+        }
+        $image->image_link = implode(',',$a);
+        $image->save();
+    }
 
-
+    return response()->json([
+        'message'=>'Xong roài đóa anh troai',
+    ]);
+});
 
 
